4f74a9a135e53386d29129ae7cb073d9e0c7deac
unknown <lvalentim@ua.pt>
2021-05-09 22:28:41 +0100

    testes models

--- Projetos/TQS_AirQuality/src/test/java/Project/TQS_AirQuality/Models/ResponseTest.java
+++ Projetos/TQS_AirQuality/src/test/java/Project/TQS_AirQuality/Models/ResponseTest.java
@@ -1,7 +1,38 @@
 package Project.TQS_AirQuality.Models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResponseTest {
+
+    private Response r;
+    private Weather[] data;
+    private long timeNow;
+    private Weather weather;
+
+    @BeforeEach
+    public void setup(){
+        r = new Response();
+    }
+
+    @Test
+    public void setData_getData(){
+        weather = new Weather();
+        data = new Weather[1];
+        data[0] = weather;
+        r.setData(data);
+        assertEquals(data, r.getData());
+    }
+
+    @Test
+    public void setTimestamp_getTimestamp(){
+        timeNow = new Timestamp(System.currentTimeMillis()).getTime();
+        r.setTimestamp(timeNow);
+        assertEquals(timeNow, r.getTimestamp());
+    }
 
 }
--- Projetos/TQS_AirQuality/src/test/java/Project/TQS_AirQuality/Models/WeatherTest.java
+++ Projetos/TQS_AirQuality/src/test/java/Project/TQS_AirQuality/Models/WeatherTest.java
@@ -1,7 +1,96 @@
 package Project.TQS_AirQuality.Models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WeatherTest {
 
+    private Weather w;
+
+    @BeforeEach
+    public void setup(){
+        w = new Weather();
+    }
+
+    @Test
+    public void setMoldLevel_getMoldLevel(){
+        w.setMold_level(1);
+        assertEquals(1, w.getMold_level());
+    }
+
+    @Test
+    public void setAqi_getAqi(){
+        w.setAqi(1.1);
+        assertEquals(1.1, w.getAqi());
+    }
+
+    @Test
+    public void setPm25_getPm25(){
+        w.setPm25(1.1);
+        assertEquals(1.1, w.getPm25());
+    }
+
+    @Test
+    public void setPredominant_pollen_type_getPredominant_pollen_type(){
+        w.setPredominant_pollen_type("grass");
+        assertEquals("grass" , w.getPredominant_pollen_type());
+    }
+
+    @Test
+    public void setSo2_getSo2(){
+        w.setSo2(1.1);
+        assertEquals(1.1, w.getSo2());
+    }
+
+    @Test
+    public void setPm10_getPm10(){
+        w.setPm10(1.1);
+        assertEquals(1.1, w.getPm10());
+    }
+
+    @Test
+    public void setCo_getCo(){
+        w.setCo(1.1);
+        assertEquals(1.1, w.getCo());
+    }
+
+    @Test
+    public void setNo2_getNo2(){
+        w.setNo2(1.1);
+        assertEquals(1.1, w.getNo2());
+    }
+
+    @Test
+    public void setO3_getO3(){
+        w.setO3(1.1);
+        assertEquals(1.1, w.getO3());
+    }
+
+    @Test
+    public void setPollen_level_weed_getPollen_level_weed(){
+        w.setPollen_level_weed(1);
+        assertEquals(1, w.getPollen_level_weed());
+    }
+
+    @Test
+    public void setPollen_level_grass_getPollen_level_grass(){
+        w.setPollen_level_grass(1);
+        assertEquals(1, w.getPollen_level_grass());
+    }
+
+    @Test
+    public void setPollen_level_tree_getPollen_level_tree(){
+        w.setPollen_level_tree(1);
+        assertEquals(1, w.getPollen_level_tree());
+    }
+
+    @Test
+    public void setCity_name_getCity_name(){
+        w.setCity_name("Porto");
+        assertEquals("Porto", w.getCity_name());
+    }
 }
